90dfc7f0bb122f9b3d80921ac03bf62db245b2ba
Pavel Drobny <32716471+paveldrobny@users.noreply.github.com>
2021-09-30 08:23:00 +0300

    update

--- event.js
+++ event.js
@@ -208,11 +208,11 @@ const checkCourse = () => {
     if (checkTime(date, startFirstPair, endFirstPair)) {
       loadLink("");
     } else if (checkTime(date, startSecondPair, endSecondPair)) {
-      loadLink("");
+      loadLink("http://e.adidonntu.ru/course/view.php?id=498");
     } else if (checkTime(date, startThirdPair, endThirdPair)) {
-      loadLink("");
+      loadLink("http://e.adidonntu.ru/course/view.php?id=461");
     } else if (checkTime(date, startFourthPair, endFourthPair)) {
-      loadLink("");
+      loadLink("http://e.adidonntu.ru/course/view.php?id=498");
     }
   }
   //////////////////////////////////////////////////////////////
