b45bdeb01a2763e469224657df02f5b62fde86e3
Pavel Drobny <32716471+paveldrobny@users.noreply.github.com>
2022-09-13 12:47:53 +0300

    Update database.js

--- database.js
+++ database.js
@@ -1,2 +1,2 @@
-export const USER_KEYS = ["desktop-5ifdkj5pavelamdfx(tm)-8300eight-coreprocessor8"];
+export const USER_KEYS = ["desktop-5ifdkj5pavelamdfx(tm)-8300eight-coreprocessor8", "desktop-h31qripmailfintel(r)core(tm)i3-6100cpu@3.70ghz4"];
 export const USER_KEYS_BLOCK = [""];
