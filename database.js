06551ed348563fed5dadd4c474e6878a608b05d4
Pavel Drobny <32716471+paveldrobny@users.noreply.github.com>
2024-11-09 23:14:35 +0300

    Update database.js

--- database.js
+++ database.js
@@ -1,2 +1,2 @@
-export const USER_KEYS = ["desktop-fuev6dvpavel12thgenintel(r)core(tm)i5-12400f12"];
+export const USER_KEYS = ["desktop-fuev6dvpavel12thgenintel(r)core(tm)i5-12400f12", "desktop-fuev6dvpavel12thgenintel(r)core(tm)i5-12400f12"];
 export const USER_KEYS_BLOCK = [""];
